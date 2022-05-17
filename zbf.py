257d7ae9098d8bfb37eb344090d6e3b4966f2cf3
alienisme123 <alienisme123@gmail.com>
2022-05-17 14:07:29 +0800

    1.02 (add vector and distance)

--- zbf.py
+++ zbf.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+from tkinter import Y
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,10 +12,15 @@ class zbf:
         self.frame = len(self.tra)
         self.dtcm = dtcm
         self.dt = dt
-
+        self.vec = np.empty((self.frame-1,self.noa,2))       #vector    time/number of animal/dimension
+        self.vec_s= np.empty((self.frame-1,self.noa,2))      #vector sum
+        self.vec_rms = np.empty((self.frame-1,self.noa))     #the rms of vector sum 
+        
+        
     def getra(self):
         return self.tra
 
+    
     # plot all fish tragetory
     def zbfplt(self):
 
@@ -57,6 +64,84 @@ class zbf:
                 plt.ylabel("Bit")
                 plt.xlabel("Time lag (sec)")
                 #plt.savefig("fish "+str(i+1)+" and "+str(j+1)+".jpg",dpi=300)
+    
+    #caculate two fishes MI
+    def MI_two(self,i,j,dim=0,time=0):
+        time = int(time/self.dt) if time else self.frame 
+        dms = range(-99,100)
+        time_lag = np.array(dms)*self.dt 
+        mi_11 = [_mi_quick(self.tra[1:time, i, dim],self.tra[1:time, i, dim], d) for d in dms]  # auto
+        mi_22 = [_mi_quick(self.tra[1:time, j, dim],self.tra[1:time, j, dim], d) for d in dms]
+        mi_12 = [_mi_quick(self.tra[1:time, i, dim],self.tra[1:time, j, dim], d) for d in dms]  # cross
+        plt.plot(time_lag, mi_11)
+        plt.plot(time_lag, mi_22)
+        # too small, needed to magnification
+        plt.plot(time_lag, np.array(mi_12)*3)
+
+        plt.title("fish "+str(i+1)+" and "+str(j+1))
+        plt.ylabel("Bit")
+        plt.xlabel("Time lag (sec)")    
+    
+    #vector and vector sum caculator
+    def zbfvec(self):
+        for i in range(0,self.noa):
+            for j in range(0,self.frame-1):
+                self.vec[j,i]=[self.tra[j+1,i,0]-self.tra[j,i,0],self.tra[j+1,i,1]-self.tra[j,i,1]]
+                self.vec_s[j,i]=[self.tra[j+1,i,0]-self.tra[1,i,0],self.tra[j+1,i,1]-self.tra[1,i,1]]
+                self.vec_rms[j,i]= np.sqrt((self.vec_s[j,i,0]**2+self.vec_s[j,i,0]**2))
+    
+    #vec plot 
+    def zbfvecplt(self,typ="rms",dim=0):
+        time=np.linspace(0,(self.frame-1)/30,self.frame-1)
+        
+        if (typ== "rms"):
+            for i in range(0,self.noa):
+                plt.figure(i)
+                plt.title(str(i+1)+" fish vector rms")
+                plt.xlabel("time (sec)")
+                plt.ylabel("vector size")
+                plt.plot(time,self.vec_rms[:,i])
+                
+        elif (typ=="sum"):
+            for i in range(0,self.noa):
+                plt.figure(i)
+                plt.xlabel("time (sec)")
+                plt.ylabel("vector size")
+                plt.title(str(i+1)+" fish vector sum in y axis" if dim else str(i+1)+" fish vector sum in x axis")
+                plt.plot(time,self.vec_s[:,i,dim])
+            
+        elif (typ=="vec"):
+            for i in range(0,self.noa):
+                plt.figure(i)
+                plt.xlabel("time (sec)")
+                plt.ylabel("vector size")
+                plt.title(str(i+1)+" fish vector in y axis" if dim else str(i+1)+" fish vector in y axis")
+                plt.plot(time,self.vec_s[:,i,dim])
+    
+    
+    #two fishes distance 
+    def zbfdis(self):
+        #set a list of two different number 
+        x = np.arange(0,self.noa)
+        comb = combinations(x,2)
+        y = list(comb)              
+    
+        time=np.linspace(0,self.frame*self.dt,self.frame)
+        dis=np.empty((self.frame,len(y)))
+        
+        n=0
+        for j in y:
+            for i in range(0,self.frame):
+                 dis[i-1,n]=np.sqrt((self.tra[i,j[0],0]-self.tra[i,j[1],0])**2+(self.tra[i,j[0],1]-self.tra[i,j[1],1])**2)/self.dtcm
+
+            plt.figure(n)
+            plt.title(str(j[0])+"-"+str(j[1]))
+            plt.xlabel("time (sec)")
+            plt.ylabel("distance (cm)")
+            plt.plot(time,dis[:,n])
+            n+=1
+
+
 
 
 # interplate
